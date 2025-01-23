4525964156e4e86c85cece077e50f487077370ef
Jan Ramil Intong <bisoyjan@gmail.com>
2025-01-23 21:49:10 +0800

    validating and storing

---
+++ task-list/resources/views/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', 'Add task')
+
+@section('content')
+    {{ $errors }}
+
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+
+        <div>
+            <label for="title">
+                Title
+            </label>
+            <input type="text" name="title" id="title" />
+        </div>
+
+        <div>
+            <label for="description">
+                Description
+            </label>
+            <textarea rows="5" name="description" id="description"></textarea>
+        </div>
+
+        <div>
+            <label for="long_description">
+                Long Description
+            </label>
+            <textarea rows="10" name="long_description" id="long_description"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
