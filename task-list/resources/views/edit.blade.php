b47e0b0f1bc7f07af99f24483ed5e4946eca89fc
Jan Ramil Intong <bisoyjan@gmail.com>
2025-01-23 23:11:11 +0800

    Edit form function

--- task-list/resources/views/edit.blade.php
+++ task-list/resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Add task')
+@section('title', 'Edit task')
 
 @section('styles')
     <style>
@@ -13,14 +13,15 @@
 
 @section('content')
 
-    <form method="POST" action="{{ route('tasks.store') }}">
+    <form method="POST" action="{{ route('tasks.update', ['id' => $task->id]) }}">
         @csrf
+        @method('PUT')
 
         <div>
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title" value="{{ $task->title }}"/>
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -30,7 +31,7 @@
             <label for="description">
                 Description
             </label>
-            <textarea rows="5" name="description" id="description"></textarea>
+            <textarea rows="5" name="description" id="description">{{ $task->description }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -40,14 +41,14 @@
             <label for="long_description">
                 Long Description
             </label>
-            <textarea rows="10" name="long_description" id="long_description"></textarea>
+            <textarea rows="10" name="long_description" id="long_description">{{ $task->long_description }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
 
         <div>
-            <button type="submit">Add Task</button>
+            <button type="submit">Update Task</button>
         </div>
     </form>
 @endsection
