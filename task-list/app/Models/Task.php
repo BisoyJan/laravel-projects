3883daf1aac36fd5fb932fa8c2a0a09073d53689
Jan Ramil Intong <bisoyjan@gmail.com>
2025-01-24 22:17:15 +0800

    reusability route model binding

--- task-list/app/Models/Task.php
+++ task-list/app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'long_description'
+    ];
+
+
 }
