ecfa9626cca56e4a7f2f21a9662e0fb944f5afb4
Jan Ramil Intong <bisoyjan@gmail.com>
2025-01-29 22:49:20 +0800

    Refactor styles for task list and detail views using Tailwind CSS classes

--- task-list/resources/views/show.blade.php
+++ task-list/resources/views/show.blade.php
@@ -3,44 +3,46 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{ $task->description }}</p>
+
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}"
+            class="link">&larr; Go Back to the task list</a>
+    </div>
+
+    <p class="mb-4 text-slate-700">{{ $task->description }}</p>
 
     @if ($task->long_description)
-        <p>{{ $task->long_description }}</p>
+        <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
     @endif
 
-    <p>{{ $task->created_at }}</p>
-    <p>{{ $task->updated_at }}</p>
+    <p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans() }} &#x2022; Updated {{ $task->updated_at->diffForHumans() }}</p>
 
-    <p>
+    <p class="mb-4">
         @if ($task->completed)
-            <span>Completed</span>
+            <span class="font-medium text-green-500">Completed</span>
         @else
-            <span>Incomplete</span>
+            <span class="font-medium text-red-500">Incomplete</span>
         @endif
     </p>
 
-    <div>
+    <div class="flex gap-2">
         {{-- you can also do this way that specifies the id but laravel is smart enough the it only needs the ID
         <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">Edit</a> --}}
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
-    </div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}"
+            class="btn">Edit</a>
 
-    <div>
         <form method="POST" action=" {{ route('tasks.toggle-complete', ['task' => $task]) }}">
             @csrf
             @method('PUT')
-            <button type="submit">
+            <button type="submit" class="btn">
                 Mark as {{ $task->completed ? 'incomplete' : 'complete' }}
             </button>
         </form>
-    </div>
 
-    <div>
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit">Delete</button>
+            <button type="submit" class="btn">Delete</button>
         </form>
     </div>
 @endsection
