7d2cc0fc65044131bfe1723261ee5312a10a9a6e
Jan Ramil Intong <bisoyjan@gmail.com>
2025-03-08 04:00:56 +0800

    Add ReviewController and create review form for books done project number 2 ```

---
+++ book-review/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class ReviewController extends BaseController
+{
+
+    /**
+     * The function sets a middleware throttle named 'reviews' for the 'store' method.
+     */
+    public function __construct()
+    {
+        $this->middleware('throttle:reviews')->only(['store']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Book $book)
+    {
+        return view('books.reviews.create', ['book' => $book]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Book $book)
+    {
+        $data = $request->validate([
+            'review' => 'required||min:15',
+            'rating' => 'required|numeric|between:1,5',
+        ]);
+
+        $book->reviews()->create($data);
+
+        return redirect()->route('books.show', $book);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,10 @@ Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
