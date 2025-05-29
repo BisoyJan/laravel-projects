5138f266102b6e028b55f092c4336a851fd02b7a
Jan Ramil Intong <bisoyjan@gmail.com>
2025-05-30 05:32:25 +0800

    feat: Enhance CreatePoll component with validation and dynamic option management

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
-    public $options = [''];
+    public $options = ['First'];
+
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255'
+    ];
+
+    protected $messages = [
+        'title.required' => 'The poll title is required.',
+        'title.min' => 'The poll title must be at least 3 characters.',
+        'title.max' => 'The poll title cannot exceed 255 characters.',
+        'options.required' => 'At least one option is required.',
+        'options.array' => 'Options must be an array.',
+        'options.min' => 'You must provide at least one option.',
+        'options.max' => 'You cannot have more than 10 options.',
+        'options.*' => 'The option can\'t be empty.',
+        'options.*.min' => 'The option must be at least 1 character long.',
+        'options.*.max' => 'The option cannot exceed 255 characters.'
+    ];
 
     public function render()
     {
@@ -17,6 +37,43 @@ class CreatePoll extends Component
     public function addOption()
     {
         $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options); // Re-index the array
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function createPoll()
+    {
+        $this->validate();
+
+        Poll::create([
+            'title' => $this->title
+        ])->options()->createMany(
+                collect($this->options)
+                    ->map(fn($option) => ['name' => $option])
+                    ->all()
+            );
+
+        // Alternatively, if you want to create options one by one:
+        // $poll = Poll::create([
+        //     'title' => $this->title
+        // ]);
+
+        // foreach ($this->options as $optionName) {
+        //     $poll->options()->create([
+        //         'name' => $optionName
+        //     ]);
+        // }
+
+        $this->reset(['title', 'options']);
     }
 
     // public function mount()
