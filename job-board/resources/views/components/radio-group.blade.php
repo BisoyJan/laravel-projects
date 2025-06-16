eb324c22ad186fd8b014610b48fd66adb7de3037
Jan Ramil Intong <bisoyjan@gmail.com>
2025-06-17 01:13:37 +0800

    feat: Implement My Jobs management; add MyJobController, employer middleware, and views for job creation and listing

--- job-board/resources/views/components/radio-group.blade.php
+++ job-board/resources/views/components/radio-group.blade.php
@@ -1,11 +1,14 @@
 <div>
-    <label for="{{ $name }}-all" class="mb-1 flex items-center">
-        <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) id="{{$name}}-all" />
-        <span class="ml-2">All</span>
-    </label>
+    @if ($allOption)
+        <label for="{{ $name }}-all" class="mb-1 flex items-center">
+            <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) id="{{$name}}-all" />
+            <span class="ml-2">All</span>
+        </label>
+    @endif
+
     @foreach ($options as $option)
         <label for="{{ $name }}-{{ $option }}" class="mb-1 flex items-center">
-            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request($name))
+            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === ($value ?? request($name)))
                 id="{{ $name }}-{{$option}}" />
             <span class="ml-2">{{Str::ucfirst($option)}}</span>
         </label>
