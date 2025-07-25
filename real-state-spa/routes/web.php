2c5a73b8dd72bb921403bf4df7626e74d9ff94c4
Jan Ramil Intong <bisoyjan@gmail.com>
2025-07-26 01:42:09 +0800

    Added index controller, implemented routes and props

--- real-state-spa/routes/web.php
+++ real-state-spa/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Index/Index');
-});
+// Route::get('/', function () {
+//     return Inertia::render('Index/Index');
+// });
 
 // Route::get('dashboard', function () {
 //     return Inertia::render('Dashboard');
@@ -14,3 +15,6 @@ Route::get('/', function () {
 // require __DIR__.'/settings.php';
 // require __DIR__.'/auth.php';
 
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/hello', [IndexController::class, 'show']);
