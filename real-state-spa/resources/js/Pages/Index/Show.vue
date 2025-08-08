ecf2d00bc2e829c536a3d2df6f7f5f262be6319c
BisoyJan <bisoyjan@gmail.com>
2025-08-09 05:13:13 +0800

    feat: Update Index and Show components; implement IndexController for routing and message handling

--- real-state-spa/resources/js/Pages/Index/Show.vue
+++ real-state-spa/resources/js/Pages/Index/Show.vue
@@ -1,1 +1,8 @@
-<template>Testing</template>
+<template>
+    <div>Show</div>
+    <Link href="/">Go to Main</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
