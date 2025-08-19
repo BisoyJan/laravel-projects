49997c885b56342511c7bda6894a77cf3b2160d0
BisoyJan <bisoyjan@gmail.com>
2025-08-20 05:18:22 +0800

    feat: custom price formatting component

---
+++ real-state-spa/resources/js/Components/UI/Price.vue
@@ -0,0 +1,20 @@
+<template>
+  <span> {{ formattedPrice }} </span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+    price: [Number, String],
+})
+
+//This format the price based on the locale of the user and the currency
+const formattedPrice = computed(
+    () => Number(props.price).toLocaleString('en-US',
+        {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+        }),
+)
+</script>
