08763a4aeab73c91ed79956fc958e63ee9934538
BisoyJan <bisoyjan@gmail.com>
2025-08-21 23:46:38 +0800

    feat: implemented composables - reusable login in monthly payment

--- real-state-spa/resources/js/Pages/Listing/Show.vue
+++ real-state-spa/resources/js/Pages/Listing/Show.vue
@@ -15,9 +15,28 @@
 
       <Box>
         <template #header>
-          Offer
+          Monthly Payment
         </template>
-        Make an offer
+        <div>
+          <label for="label">Interest rate  ({{ interestRate }} %)</label>
+          <input
+            v-model.number="interestRate"
+            type="range" min="0.1" max="30" step="0.1"
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
+
+          <label for="label">Duration ({{ duration }} Years)</label>
+          <input
+            v-model.number="duration"
+            type="range" min="3" max="35" step="1"
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
+        </div>
+
+        <div class="text-gray-600 dark:text-gray-300 mt-2">
+          <div class="text-gray-400">Your monthly payment</div>
+          <Price :price="monthlyPayment" class="text-3xl" />
+        </div>
       </Box>
     </div>
   </div>
@@ -29,7 +48,16 @@ import Box from '@/Components/UI/Box.vue'
 import ListingSpace from '@/Components/UI/ListingSpace.vue'
 import Price from '@/Components/UI/Price.vue'
 
-defineProps({
+import { ref } from 'vue'
+import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
     listing: Object,
 })
+
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration)
+
 </script>
