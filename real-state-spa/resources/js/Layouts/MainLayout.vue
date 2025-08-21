61dac111baa64f3e09ab4859185e261a64774628
BisoyJan <bisoyjan@gmail.com>
2025-08-21 22:55:44 +0800

    feat: Extracting Tailwind style using @apply directive

--- real-state-spa/resources/js/Layouts/MainLayout.vue
+++ real-state-spa/resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
@@ -16,7 +16,7 @@
   </header>
 
   <main class="container mx-auto p-4">
-    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-300 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+    <div v-if="flashSuccess" class="btn-primary">
       {{ flashSuccess }}
     </div>
     <slot>Default</slot>
