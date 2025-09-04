1759fef83a805da1bfa7547a635933709a7bf9c1
BisoyJan <bisoyjan@gmail.com>
2025-09-04 23:07:54 +0800

    feat: display current listing images in the upload form

--- real-state-spa/app/Models/ListingImage.php
+++ real-state-spa/app/Models/ListingImage.php
@@ -11,9 +11,18 @@ class ListingImage extends Model
     use HasFactory;
 
     protected $fillable = [ 'filename' ];
+    protected $appends = ['src'];
 
     public function listing(): BelongsTo
     {
         return $this->belongsTo(Listing::class);
+    }
+
+    public function getSrcAttribute()
+    {
+        if (\Storage::disk('public')->exists($this->filename)) {
+            return \Storage::url($this->filename);
+        }
+        return null;
     }
 }
