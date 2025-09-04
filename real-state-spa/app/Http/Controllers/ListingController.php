529f6bf957b8d48e50bcd1911db2079223a3af7f
BisoyJan <bisoyjan@gmail.com>
2025-09-05 02:48:24 +0800

    feat: load and display images for listings in the show view

--- real-state-spa/app/Http/Controllers/ListingController.php
+++ real-state-spa/app/Http/Controllers/ListingController.php
@@ -63,6 +63,7 @@ class ListingController extends Controller
         // }
 
         //$this->authorize('view', $listing);
+        $listing->load('images');
 
         return inertia(
             'Listing/Show',
